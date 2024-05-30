c8dbc166caabbb8a765ff28f13c08b8596a1f399
jeessicats <jeessicats@gmail.com>
2024-05-29 21:41:21 -0300

    projeto candidatura

---
+++ controle-fluxo/src/FormatarCep.java
@@ -0,0 +1,18 @@
+public class FormatarCep {
+    public static void main(String[] args) {  
+        try {
+            String cepFormatado =  formatarCep ("2376564");
+            System.out.println(cepFormatado);
+        } catch (CepInvalidoException e) {
+            // TODO Auto-generated catch block
+            System.out.println("O cep não contem oito digitos");
+        }
+    }
+    static String formatarCep(String cep) throws CepInvalidoException{
+        if(cep.length() != 8)
+          throw new CepInvalidoException();
+        
+          //simulando um cep formatado
+          return "23.765-064";
+    }
+}
