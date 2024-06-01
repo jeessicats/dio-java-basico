773e65aaa46094ad1834fb8dfe3993eaa56b2d50
jeessicats <jeessicats@gmail.com>
2024-06-01 12:35:25 -0300

    commit do desafio de fluxo

--- desafio-controle-fluxo/src/Contador.java
+++ desafio-controle-fluxo/src/Contador.java
@@ -4,10 +4,10 @@ public class Contador {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Digite o primeiro número:");
+        System.out.println("Digite o primeiro número: ");
         int primeiroNumero = scanner.nextInt();
 
-        System.out.println("Digite o segundo número:");
+        System.out.println("Digite o segundo número: ");
         int segundoNumero = scanner.nextInt();
 
         try {
