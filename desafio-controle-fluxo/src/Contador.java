fde1d624a1219fe74920e53618476a343c435564
jeessicats <jeessicats@gmail.com>
2024-06-01 12:31:28 -0300

    commit desafio-controle-fluxo

---
+++ desafio-controle-fluxo/src/Contador.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Contador {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o primeiro número:");
+        int primeiroNumero = scanner.nextInt();
+
+        System.out.println("Digite o segundo número:");
+        int segundoNumero = scanner.nextInt();
+
+        try {
+            contar(primeiroNumero, segundoNumero);
+        } catch (ParametrosInvalidosException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            scanner.close();
+        }
+    }
+
+    public static void contar(int primeiroNumero, int segundoNumero) throws ParametrosInvalidosException {
+        if (primeiroNumero >= segundoNumero) {
+            throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro!");
+        }
+
+        int diferenca = segundoNumero - primeiroNumero;
+        for (int i = 1; i <= diferenca; i++) {
+            System.out.println("Imprimindo o número " + i);
+        }
+    }
+}
